9d7fb29747ddbbfad83374abdf8ab03fb37a6fb3
AngeloRLongoria <arlongoria93@gmail.com>
2020-10-01 17:09:01 -0500

    Dynamic
    -Removed movie/details page
    -added movie data structure js
    -dynamically added movie data into webpage with onclicks.

--- js/MOVIE_DATA/MOVIE_DATA.js
+++ js/MOVIE_DATA/MOVIE_DATA.js
@@ -2,10 +2,63 @@ const MOVIE_DATA = [
   {
     id: 1,
     title: "Goodfellas",
+    rank: 1,
     year: 1990,
+    img: "../../img/movie-covers/goodfellas.jpg",
+    rrg: "R | 146 min | Biography, Crime, Drama",
     description:
       "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
     videolink: "http://idk.xyz",
+  },
+  {
+    id: 2,
+    rank: 2,
+    title: "Once Upon a Time in America",
+    year: 1984,
+    img: "../../img/movie-covers/once-upon-a-time-in-america.jpg",
+    rrg: "R | 229 min | Crime, Drama",
+    description:
+      "A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life.",
+  },
+  {
+    id: 3,
+    rank: 3,
+    title: "The Godfather",
+    year: 1972,
+    img: "../../img/movie-covers/godfather.jpg",
+    rrg: "R | 175 min | Crime, Drama",
+    description:
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+  },
+  {
+    id: 4,
+    rank: 4,
+    title: "The Godfather: Part II",
+    year: 1974,
+    img: "../../img/movie-covers/godfather-part2.jpg",
+    rrg: "R | 202 min | Crime, Drama",
+    description:
+      "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+  },
+  {
+    id: 5,
+    rank: 5,
+    title: "Pulp Fiction",
+    year: 1994,
+    img: "../../img/movie-covers/pulp-fiction.jpg",
+    rrg: "R | 154 min | Crime, Drama",
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+  },
+  {
+    id: 6,
+    rank: 6,
+    title: "The Departed",
+    year: 2006,
+    img: "../../img/movie-covers/the-departed.jpg",
+    rrg: "R | 151 min | Crime, Drama, Thriller",
+    description:
+      "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
   },
 ];
 export default MOVIE_DATA;
---
+++ js/pageB.js
@@ -0,0 +1,6 @@
+import MOVIE_DATA from "./MOVIE_DATA/MOVIE_DATA.js";
+import { goodfellas } from "./main.js";
+console.log(goodfellas);
+// const goodfellass = MOVIE_DATA[0];
+// let innerP = document.getElementById("innerText");
+// console.log(innerP);
